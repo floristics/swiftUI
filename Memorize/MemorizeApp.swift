50032c41b375d6122c887c44d4f35cbf85d36bb5
Pavel Rodionov <pafi2010@ya.ru>
2022-06-09 11:36:49 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Павел Родионов on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
