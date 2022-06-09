189430d0944f1038fc16cd51ac2604172d03e07c
Pavel Rodionov <pafi2010@ya.ru>
2022-06-09 13:54:38 +0700

    first lesson progress

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            
+            Text("Hello, world!")
+                .foregroundColor(.orange)
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
     }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        return ContentView()
     }
 }
